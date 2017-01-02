509295f8baf0d5fc2848246122f9c6a01ba56a0e
StudenetskiyA <wow.dayre@gmail.com>
2017-01-02 18:41:42 +0500

    Кнопка конца хода.

--- com/company/Main.java
+++ com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
 
+import sun.misc.JavaLangAccess;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -20,15 +22,16 @@ public class Main extends JFrame{
     private static double CARD_PROP = 1.4;
     private static double CARD_SIZE_FROM_SCREEN = 0.12;
 
-    private static Main main;
+    private static Main main = new Main();
 
-    private static ViewField viewField;
-    private static JLabel deckClick;
+    private static ViewField viewField = new ViewField();
+    private static JLabel deckClick = new JLabel();
     private static JLabel cardClick[]= new JLabel[9];
     private static JLabel playerUnitClick[]= new JLabel[9];
     private static JLabel battlegroundClick = new JLabel();
-    private static JLabel playerCoinLabel;
-    public static JLabel gameLog;
+    private static JLabel playerCoinLabel = new JLabel();
+    public static JLabel gameLog=new JLabel();
+    private static JLabel endTurnClick=new JLabel();
 
     private static Image background;
     private static Image heroImage;
@@ -36,7 +39,7 @@ public class Main extends JFrame{
     private static Image heroCoinImage;
     private static Image enemyCoinImage;
     private static Image heroDeckImage;
-
+private static Image endTurnImage;
     private static Image heroGraveyardImage;
     private static Image enemyDeckImage;
     private static Image enemyGraveyardImage;
@@ -56,23 +59,19 @@ public class Main extends JFrame{
             enemyCoinImage = ImageIO.read(Main.class.getResourceAsStream("Coin.png"));
             heroDeckImage = ImageIO.read(Main.class.getResourceAsStream("Deck.png"));
             enemyDeckImage = ImageIO.read(Main.class.getResourceAsStream("Deck.png"));
+            endTurnImage = ImageIO.read(Main.class.getResourceAsStream("Endturn.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        main = new Main();
         main.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         main.setLocation(0,0);
         main.setSize(1300,700);
 
-        viewField = new ViewField();
-
-        playerCoinLabel = new JLabel();
         playerCoinLabel.setLocation(0,0);
         playerCoinLabel.setSize(1,1);
         playerCoinLabel.setHorizontalAlignment(SwingConstants.LEFT);
         playerCoinLabel.setVerticalAlignment(SwingConstants.TOP);
         playerCoinLabel.setForeground(Color.WHITE);
-        gameLog = new JLabel();
         gameLog.setLocation(0,0);
         gameLog.setSize(250,700);
         gameLog.setHorizontalAlignment(SwingConstants.LEFT);
@@ -81,10 +80,9 @@ public class Main extends JFrame{
 
         viewField.setLayout(new BorderLayout());
 
-        deckClick = new JLabel("");
         deckClick.addMouseListener(mouseListener(1,0));
-        battlegroundClick = new JLabel();
         battlegroundClick.addMouseListener(mouseListener(3,0));
+        endTurnClick.addMouseListener(mouseListener(9,0));
 
         for (int i=0;i<cardClick.length;i++){
             cardClick[i]=new JLabel();
@@ -100,6 +98,7 @@ public class Main extends JFrame{
         viewField.add(battlegroundClick);
         viewField.add(deckClick);
         viewField.add(gameLog);
+        viewField.add(endTurnClick);
         viewField.add(playerCoinLabel);
 
         main.add(viewField);
@@ -144,6 +143,7 @@ public class Main extends JFrame{
             @Override
             public void mouseClicked(MouseEvent e) {
                 if (onWhat==1)  player.drawCard();
+                if (onWhat==9) player.newTurn();
             }
 
             @Override
@@ -175,9 +175,13 @@ public class Main extends JFrame{
         };
     }
 
-
     private static void onRepaint(Graphics g){
+
+        playerCoinLabel.setText(player.untappedCoin+"/"+player.totalCoin);
        g.drawImage(background,0,0, main.getWidth(),main.getHeight(),null);
+       g.drawImage(endTurnImage,main.getWidth()- B0RDER_RIGHT - endTurnImage.getWidth(null),(int)(main.getHeight()/2),null);
+        endTurnClick.setLocation(main.getWidth()- B0RDER_RIGHT - endTurnImage.getWidth(null),(int)(main.getHeight()/2));
+        endTurnClick.setSize(endTurnImage.getWidth(null),endTurnImage.getHeight(null));
        g.drawRect(100,200,600,200);
        int heroW = (int)(main.getWidth()*CARD_SIZE_FROM_SCREEN);
        int heroH = (int)(heroW*CARD_PROP);
@@ -189,6 +193,7 @@ public class Main extends JFrame{
 
        deckClick.setLocation(smallCardW*2+B0RDER_LEFT+B0RDER_BETWEEN,main.getHeight()-smallCardH-B0RDER_BOTTOM);
        deckClick.setSize(smallCardW,smallCardH);
+
        g.drawImage(enemyImage,main.getWidth()-heroW-B0RDER_RIGHT,B0RDER_TOP,heroW,heroH,null);
        g.drawImage(enemyCoinImage,smallCardW*3+B0RDER_LEFT+B0RDER_BETWEEN,B0RDER_TOP,smallCardW,smallCardH,null);
 
